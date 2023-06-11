cd8d44ef729bddc84b7f4c634c75261946cb72cb
Pavel Kulik <Kanstantin0105@gmail.com>
2023-06-11 22:10:15 +0300

    task_1 add

---
+++ task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+bool isData(int m, int n)
+{
+    return m < n;
+}
+
+void NumbersBetween(int start, int end)
+{
+    Console.Write($" {start}");
+    if (start < end)
+        NumbersBetween(start + 1, end);        
+}
+
+Console.Write("Введите число: ");
+int left = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int right = int.Parse(Console.ReadLine()!);
+if (isData(left, right))
+{
+    Console.Write($"M = {left}; N = {right}. ->");
+    NumbersBetween(left, right);
+}
+else
+    Console.WriteLine($"{left} > {right}");
