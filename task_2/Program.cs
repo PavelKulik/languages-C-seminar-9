708c9cf79f3dedcbf777d16a296a1e1dd30fed11
Pavel Kulik <Kanstantin0105@gmail.com>
2023-06-11 22:19:11 +0300

    Task_2 add

---
+++ task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+bool isData(int m, int n)
+{
+    return m < n;
+}
+
+int SumNumbersBetween(int start, int end)
+{
+    if (start < end)
+        return start + SumNumbersBetween(start + 1, end); 
+    else
+        return start;
+}
+
+Console.Write("Введите число: ");
+int left = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int right = int.Parse(Console.ReadLine()!);
+if (isData(left, right))
+{
+    Console.Write($"M = {left}; N = {right}. -> {SumNumbersBetween(left, right)}");
+    
+}
+else
+    Console.WriteLine($"{left} > {right}");
