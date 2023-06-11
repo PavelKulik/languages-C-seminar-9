fae7bfeecf31e13a234e690a3f23680fe8d54e9b
Pavel Kulik <Kanstantin0105@gmail.com>
2023-06-11 22:37:52 +0300

    task_3 add

---
+++ task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿bool isData(int m, int n)
+{
+    return m >= 0 || n >= 0;
+}
+
+int AckermanFunctions(int start, int end)
+{
+    if (start == 0)
+        return end + 1;
+    else
+    {
+        if (start > 0 && end == 0)
+            return AckermanFunctions(start - 1, 1);
+        else
+            return AckermanFunctions(start - 1, AckermanFunctions(start, end - 1)); 
+    }
+ 
+}
+
+Console.Write("Введите число: ");
+int left = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int right = int.Parse(Console.ReadLine()!);
+if (isData(left, right))
+    Console.Write($"M = {left}; N = {right}. -> A(m, n) = {AckermanFunctions(left, right)}");
+else
+    Console.WriteLine($"{left} or {right} < 0");
